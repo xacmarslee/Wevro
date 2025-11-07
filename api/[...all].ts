061df98c8b77f8e8ca6b8027c3175c492a8aa515
xacmarslee <xacmarslee@gmail.com>
2025-11-08 00:57:40 +0800

    fix: await express app completion in vercel handler

--- api/[...all].ts
+++ api/[...all].ts
@@ -7,7 +7,19 @@ const appPromise = createApp();
 
 export default async function handler(req: any, res: any) {
   const app = await appPromise;
-  return app(req, res);
+  return new Promise<void>((resolve, reject) => {
+    res.on("finish", resolve);
+    res.on("close", resolve);
+    res.on("error", reject);
+
+    app(req, res, (err: unknown) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
 }
 
 export const config = {
