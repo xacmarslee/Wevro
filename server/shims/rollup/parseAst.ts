efebb652db2e97f76240598c4763afb32e106a69
xacmarslee <xacmarslee@gmail.com>
2025-11-08 19:12:21 +0800

    build: alias rollup to shim directory for server bundle

---
+++ server/shims/rollup/parseAst.ts
@@ -0,0 +1,7 @@
+export function parseAst() {
+  throw new Error("rollup/parseAst is not available at runtime");
+}
+
+export async function parseAstAsync() {
+  throw new Error("rollup/parseAst is not available at runtime");
+}
